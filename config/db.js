5a2f734323b22e8e32fdc85b2e4a67dcf57e21b9
s-101 <shubhank1011@gmail.com>
2024-01-13 03:09:40 +0530

    db.js changed

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 function connectDB() {
     // Database connection 🥳
-    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true , retryWrites: true });
     const connection = mongoose.connection;
 
     connection.once('open', () => {
