d87cd7ef18a2e687115f5f7da24d916eec63300a
s-101 <shubhank1011@gmail.com>
2024-03-04 03:10:33 +0530

    modified download.ejs

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ require('dotenv').config();
 const app= express();
 const PORT=process.env.PORT||3000;
 const connectDB=require('./config/db');
+const cors=require("cors");
 const path =require('path');
 connectDB();
+
 app.use(express.json());
 app.use(express.static('public'))
 app.set('views',path.join(__dirname,'/views'));
 app.set('view engine','ejs');
+const cors_op={
+    origin:process.env.ALLOWED_CLIENTS.split(',')
 
+}
 //routes
+app.use(cors(cors_op));
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download',require('./routes/download'))
